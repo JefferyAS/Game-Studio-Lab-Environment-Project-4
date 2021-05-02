f2af7e7aae34f76f91a7c3a1f2000f97d7be0faf
JefferyAS <jeffery2001@163.com>
2021-05-02 10:02:47 -0400

    Enemy Ship System(still in progress)

---
+++ Game Enviroment Project4/Assets/Script/EnemyBulletScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+    public float survivedTime;
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime > survivedTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Obstacle")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "Player")
+        {
+            other.GetComponent<HPSystem>().LoseHealth(1);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Game Enviroment Project4/Assets/Script/ShootBullet.cs
+++ Game Enviroment Project4/Assets/Script/ShootBullet.cs
@@ -7,6 +7,7 @@ public class ShootBullet : MonoBehaviour
     public GameObject bullet;
     public float setCoolDown;
     public float startForce;
+    public PlayerRecorder playerRecorder;
     private float coolDown;
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class ShootBullet : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space)) {
                 GameObject newBullet=Instantiate(bullet,transform.position,Quaternion.identity);
                 newBullet.GetComponent<Rigidbody>().AddForce(0,0,startForce);
+                playerRecorder.RecordShooting();
                 coolDown = setCoolDown;
             }
         }
