edbe3ac935998ab285fb5dbcf9779aac371ba530
JefferyAS <jeffery2001@163.com>
2021-05-05 03:19:29 -0400

    last edit

--- Game Enviroment Project4/Assets/Script/TextTriggerByTime.cs
+++ Game Enviroment Project4/Assets/Script/TextTriggerByTime.cs
@@ -13,29 +13,34 @@ public class TextTriggerByTime : MonoBehaviour
     private bool isText1;
     private bool isText2;
     private bool isText3;
+    private float sceneTime;
+    public bool releaseCursor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sceneTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time>time1&&!isText1) {
+        if (Time.time - sceneTime > time1&&!isText1) {
             text1.ShowText();
             isText1 = true;
         }
-        if (Time.time > time2 && !isText2)
+        if (Time.time - sceneTime > time2 && !isText2)
         {
             text2.ShowText();
             isText2 = true;
         }
-        if (Time.time > time3 && !isText3)
+        if (Time.time - sceneTime > time3 && !isText3)
         {
             text3.ShowText();
             isText3 = true;
-            Screen.lockCursor = false;
+            if (releaseCursor) {
+                Screen.lockCursor = false;
+            }
+            
         }
     }
 }
